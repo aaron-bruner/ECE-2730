33a078351763669d37f46596a70060dee6214c70
Aaron Joseph Bruner <ajbrune@g.clemson.edu>
2020-09-05 15:25:53 -0400

    Lab 1 Finsihed

--- lab1/ajbrune_273_002_1.s
+++ lab1/ajbrune_273_002_1.s
@@ -5,30 +5,30 @@
 */
 /* begin assembly code */
 
-.globl asum // Tells the compiler that asum is a global lable.
-.type asum,@function // Defines our function
+.globl asum
+.type asum,@function
 asum:
-    pushl %ebp // Subtract 4 bytes from the stack pointer then write the argument to the new address
-    movl %esp, %ebp // Put whatever value is in registery %esp into registery %ebp
-    subl $4, %esp // Subtract 4 from ESP
-    movl $0, -4(%ebp) // Put 0 into the memory location EBP-4 (we allocated this in the pushl instruction)
+    pushl %ebp      // Initialize EBP as our initial sum value and make it 0
+    movl %esp, %ebp
+    subl $4, %esp
+    movl $0, -4(%ebp)
 .L2:
-    movl 8(%ebp),%eax // Move EBP+8 into the address EAX
-    cmpb $0,(%eax) // If the value in the resgisty EAX is equal to 0 then goto L4
+    movl 8(%ebp),%eax   // The section L2 seems like a for loop and L4 seems like a sub function
+    cmpb $0,(%eax)      // If the value in the resgisty EAX is equal to 0 then goto L4
     jne .L4
-    jmp .L3 // Jump to L3
+    jmp .L3             // Once we finish the 'for loop' we jump to L3
 .L4:
-    movl 8(%ebp),%eax // Move EBP+8 into the address EAX
-    movsbl (%eax),%edx // movsbl will expand EAX into a 32-bit signed integer and move it into registery EDX
-    addl %edx, -4(%ebp) // We're going to take the value of EDX and EBP-4 and add them together
-    incl 8(%ebp) // Increase EBP+8 by 1
-    jmp .L2 // Jump to L2
+    movl 8(%ebp),%eax    // Convert EAX into an integer format, then add it to our sum variable in the registry EDX
+    movsbl (%eax),%edx   // then, we return back to L2.
+    addl %edx, -4(%ebp)
+    incl 8(%ebp)         // EBP seems like a counter for the for loop in L2
+    jmp .L2
 .L3:
     movl -4(%ebp), %eax
     jmp .L1
 .L1:
     movl %ebp, %esp
-    popl %ebp
+    popl %ebp         // Put the sum variable at the top of the stack so that we can return it
     ret
 
 /* end assembly */
