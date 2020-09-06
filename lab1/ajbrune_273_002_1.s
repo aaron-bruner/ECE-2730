1cb13e795fdb8da2d2de7fbec444008a99e7fbf0
Aaron Joseph Bruner <ajbrune@g.clemson.edu>
2020-09-06 19:58:44 -0400

    Lab 1 Final

--- lab1/ajbrune_273_002_1.s
+++ lab1/ajbrune_273_002_1.s
@@ -8,27 +8,27 @@
 .globl asum
 .type asum,@function
 asum:
-    pushl %ebp      // Initialize EBP as our initial sum value and make it 0
-    movl %esp, %ebp
-    subl $4, %esp
-    movl $0, -4(%ebp)
+    pushl %ebp      // Save old base pointer for restore
+    movl %esp, %ebp // Make current stack pointer the new base pointer.
+    subl $4, %esp // Allocate 4 bytes of memory for the sum.
+    movl $0, -4(%ebp) // Initialize the sum to 0.
 .L2:
-    movl 8(%ebp),%eax   // The section L2 seems like a for loop and L4 seems like a sub function
-    cmpb $0,(%eax)      // If the value in the resgisty EAX is equal to 0 then goto L4
-    jne .L4
+    movl 8(%ebp),%eax   // Move pointer to buffer into EAX.
+    cmpb $0,(%eax)      // Dereference EAX and check if it is equal to null. (Strings are null-terminated)
+    jne .L4             // If no, then we loop by jumping to L4.
     jmp .L3             // Once we finish the 'for loop' we jump to L3
 .L4:
-    movl 8(%ebp),%eax    // Convert EAX into an integer format, then add it to our sum variable in the registry EDX
-    movsbl (%eax),%edx   // then, we return back to L2.
-    addl %edx, -4(%ebp)
-    incl 8(%ebp)         // EBP seems like a counter for the for loop in L2
+    movl 8(%ebp),%eax    // Move pointer to buffer into EAX.
+    movsbl (%eax),%edx   // Dereference EAX and move its value into EDX.
+    addl %edx, -4(%ebp)  // Add EDX to the sum.
+    incl 8(%ebp)         // Increment the buffer pointer by one. (Effectively "discarding" the current first element thus we are able to iterate through it)
     jmp .L2
 .L3:
-    movl -4(%ebp), %eax
+    movl -4(%ebp), %eax // Return the sum.
     jmp .L1
 .L1:
-    movl %ebp, %esp
-    popl %ebp         // Put the sum variable at the top of the stack so that we can return it
+    movl %ebp, %esp // Restore stack pointer to base pointer
+    popl %ebp         // Restore base pointer.
     ret
 
 /* end assembly */
